67dd07d14b016dd1b07a5eef8bd5c9cdc712e8a3
wgz <852043941@qq.com>
2020-04-01 20:43:37 +0800

    wgz分支第二次提交修改了wgz.java文件增加getset方法和tostring方法，添加构造方法

--- src/main/java/com/newer/domain/wgz.java
+++ src/main/java/com/newer/domain/wgz.java
@@ -3,4 +3,27 @@ package com.newer.domain;
 public class wgz {
 public int id;
 public String name;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public wgz(int id, String name) {
+	super();
+	this.id = id;
+	this.name = name;
+}
+@Override
+public String toString() {
+	return "wgz [id=" + id + ", name=" + name + "]";
+}
+
+
 }
